d3d1b6eb52022a465e30a9848a1a597f13d4682a
gFelicio <gustavo.a.felicio@gmail.com>
2020-09-10 23:06:55 +0000

    2020-09-10 quinta-feira 19h busca binaria recursiva

---
+++ busca_binaria_cont_copy.js
@@ -0,0 +1,70 @@
+let comp = 0
+
+function buscaBinaria (lista, valorBusca, fnComp, inicio, fim) {
+    if (fim >= inicio) {
+        let meio = Math.floor((fim + inicio) / 2)
+
+        let res = fnComp(lista[meio], valorBusca)
+
+        if (res == 0) {
+            comp++
+            return meio
+        } else if (res < 0) {
+            comp += 2
+            // fim = meio - 1
+            return buscaBinaria(lista, valorBusca, fnComp, inicio, meio - 1)
+        } else { //res > 0
+            comp += 2
+            // inicio = meio + 1
+            return buscaBinaria(lista, valorBusca, fnComp, meio + 1, fim)
+        }
+    }
+
+    return -1
+}
+
+function comparaNome(obj, valorBusca) {
+    if (valorBusca === obj.first_name) {
+        return 0
+    } else if (valorBusca < obj.first_name) {
+        return -1
+    } else {
+        return 1
+    }
+}
+
+let numeros = []
+for (let i = 0; i <= 1000; i++) {
+    numeros.push(i);
+}
+
+let nums = [4, 16, 22, 29, 35, 44, 52, 58, 66, 71, 80, 88, 94]
+
+console.log(numeros.length, 'BUSCA BINARIA RECURSIVA', buscaBinaria(numeros, 44, (elPos, busca) => {
+    if (busca == elPos) return 0
+    else if (busca < elPos) return -1
+    else return 1
+}, 0, numeros.length - 1))
+
+process.exit(0)
+
+let listaNomes = require('./dados/lista-nomes')
+
+console.time('FAUSTO')
+console.log(buscaBinaria(listaNomes, 'FAUSTO', comparaNome))
+console.timeEnd('FAUSTO')
+console.log('COMPARACOES', comp)
+
+console.log('_------------_______------------')
+
+comp = 0
+
+console.time('FAUSTO')
+console.log(buscaBinaria(listaNomes, 'FAUSTO', (obj, busca) => {
+    if (busca == obj.first_name) return 0
+    else if (busca < obj.first_name) return -1
+    else return 1
+}))
+
+console.timeEnd('FAUSTO')
+console.log('COMPARACOES', comp)
