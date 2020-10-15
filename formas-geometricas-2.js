f7a9be0d7a2166e9646e34da89189fb536faf2f0
gFelicio <gustavo.a.felicio@gmail.com>
2020-10-15 23:31:40 +0000

    classes

--- formas-geometricas-2.js
+++ formas-geometricas-2.js
@@ -1,8 +1,32 @@
 class FormaGeometrica {
     constructor (b, a, t) {
+        if (isNaN(b) || b <= 0) {
+            throw new Error('A base deve ser numérica e maior do que zero.')
+        }
+
+        if (isNaN(a) || a <= 0) {
+            throw new Error ('A altura deve ser numérica e maior do que zero.')
+        }
+
+        if (['R','T','E'].indexOf(tipo) < 0) {
+            throw new Error ('O tipo deve ser R, T ou E.')
+        }
         this.base = b
         this.altura = a
         this.tipo = t
+    }
+
+    area () {
+        switch (this.tipo) {
+            case 'R':
+                return this.base * this.altura
+            case 'T':
+                return this.base * this.altura / 2
+            case 'E':
+                return (this.base / 2) * (this.altura / 2) * Math.PI
+            case default:
+                return null
+        }
     }
 }
 
